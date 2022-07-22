0e13fea0c63cbe9b2f4b8ff89295c1a76ae481d9
ketul suthar <k2lsuthar@gmail.com>
2022-07-22 01:52:48 -0400

    Added first Rest api code

---
+++ rest-api/market_data.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type payload struct {
+	Endpoint      string                   `json:"endpoint"`
+	Quotes        []map[string]interface{} `json:"quotes"`
+	RequestedTime string                   `json:"requested_time"`
+	Timestamp     int                      `json:"timestamp"`
+}
+
+func main() {
+
+	currencies := "EURUSD,GBPUSD"
+	apiKey := "SepVs06vQMaHUkOQ8JkT"
+
+	URL := "https://marketdata.tradermade.com/api/v1/live?currency=" + currencies + "&api_key=" + apiKey
+
+	resp, err := http.Get(URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if resp.StatusCode == 200 {
+		body, err := ioutil.ReadAll(resp.Body)
+		fmt.Println()
+		if err != nil {
+			log.Fatal(err)
+		}
+		data := payload{}
+		jsonErr := json.Unmarshal(body, &data)
+		if jsonErr != nil {
+			log.Fatal(jsonErr)
+		}
+
+		fmt.Println("Endpoint:", data.Endpoint, "\nRequested Time:", data.RequestedTime, "\nTimestamp:", data.Timestamp)
+
+		for _, v := range data.Quotes {
+			fmt.Println("ask:", v["ask"])
+			fmt.Println("base_currency:", v["base_currency"])
+			fmt.Println("bid:", v["bid"])
+			fmt.Println("mid:", v["mid"])
+			fmt.Println("quote_currency:", v["quote_currency"])
+		}
+	}
+}
